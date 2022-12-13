317856cd714eec48218e7ed67197575f59e9c122
Ypd20 <101164198+Ypd20@users.noreply.github.com>
2022-12-13 18:02:09 +0530

    Update database.py

--- backend/database/database.py
+++ backend/database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/competition_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://db_username:db_password@localhost/db_name"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
